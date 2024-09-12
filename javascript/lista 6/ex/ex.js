f21adef6efce1cf0b98ccc64137a2a00a55166f5
Vitoria Alves <sophiavicky1804@gmail.com>
2024-09-12 10:49:42 -0300

    commit

--- javascript/lista 6/ex/ex.js
+++ javascript/lista 6/ex/ex.js
@@ -22,6 +22,59 @@ console.log(linesToColumns(["produto;preço", "Arroz;5.00", "Feijão;7.50"]));
 console.log(linesToColumns(["Filme;Diretor;Ano;Duração;Gênero;País de Origem;Receita Bruta", "Parasita;Bong Joon-ho;2019;132;Drama;Coréia do Sul;258.8M", "Green Book;Peter Farrelly;2018;130;Drama;EUA;321.8M"]));
 
 //Funçao 3
-function extractHeader(extract) {
-    return extract.split();
+function extractHeader(rows) {
+    if (rows.length === 0) {
+        return [];
+    }
+    return rows[0];
 }
+
+console.log(extractHeader([["nome", "idade"], ["João", "28"], ["Maria", "32"]]));
+
+console.log(extractHeader([["produto", "preço"], ["Arroz", "5.00"], ["Feijão", "7.50"]]));
+
+console.log(extractHeader([["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"],["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"],["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA", "321.8M"]
+]));
+
+//Função 4
+function extractContent (row){
+    if (row.length <= 1) {
+        return [];
+    }
+    return row.slice(1);
+}
+console.log(extractContent([["nome", "idade"], ["João", "28"], ["Maria", "32"]]));
+console.log(extractContent([["produto", "preço"], ["Arroz", "5.00"], ["Feijão", "7.50"]]));
+console.log(extractContent([["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"],["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"],["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA", "321.8M"]]));
+
+//Função 5
+function rowToJSON(header, content){
+    const obj = {};
+    
+    for(let i=0; i < header.length; i++) {
+    let key = header[i];
+    let value = content[i];
+
+    obj[key] = value;
+    }
+    return obj;
+}
+    console.log(rowToJSON(["nome", "idade"], ["João", "28"]))
+    console.log(rowToJSON(["produto", "preço"], ["Arroz", "5.00"]))
+    console.log(rowToJSON(["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"], ["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"]))
+
+//Função 6
+function contentToJsonData(header, content) {
+    const list = [];
+    for (let i = 0; i < content.length; i++) {
+        let row = content[i]
+        let obj = rowToJSON(header, row)
+        list.push(obj)
+    }
+    return list
+}
+   console.log(contentToJsonData(["nome", "idade"],[["João", "28"], ["Maria", "32"]]));
+   console.log(contentToJsonData(["produto", "preço"],[["Arroz", "5.00"], ["Feijão", "7.50"]]))
+   console.log(contentToJsonData(["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"],[["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul","258.8M"], ["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA","321.8M"]]))
+
+   
